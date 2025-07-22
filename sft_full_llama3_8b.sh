baafd4ece56c1d343f291faff2f33f220395de37
tre3x <gangulyofficialshreyan@gmail.com>
2025-07-22 09:22:08 -0500

    updated mistral code

---
+++ sft_full_llama3_8b.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copy/paste this job script into a text file and submit with:
+#    sbatch sft_full_llama3_8b.sh
+
+#SBATCH --time=240:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:a100:2
+#SBATCH --partition=nova
+#SBATCH --job-name="sft_full"
+#SBATCH --mail-user=shreyang@iastate.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --output="sbatch-logs/sft-lama3_8b-full"
+#SBATCH --error="sbatch-logs/sft-lama3_8b-full"
+
+# Activate conda (adjust path if needed)
+source /work/mech-ai/shreyan/miniconda3/etc/profile.d/conda.sh
+conda activate /work/mech-ai/shreyan/minconda3/envs/llamafactory
+
+# Change to working dir (quote paths!)
+cd "/work/mech-ai-scratch/shreyang/LLaMA-Factory"
+
+# Run your Python script with arguments
+export WANDB_PROJECT=llama3_8b_agthoughts
+export WANDB_API_KEY=271886b31cddea20b12cb5a3c7cb95718a7c16c7
+export PATH="$CONDA_PREFIX/bin:$PATH"
+llamafactory-cli train examples/train_full/llama3_8b_agthoughts_full_sft.yaml
